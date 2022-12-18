42c37724d13b6121bff3a45b85441e3173e22933
PrintingTom <52293655+PrintingTom@users.noreply.github.com>
2022-12-18 19:59:28 +0100

    Tested and it works

--- Get_All_PDF's.py
+++ Get_All_PDF's.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 18 19:37:44 2022
+import os
+import shutil
 
-@author: User
-"""
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        for name in files:
+            yield os.path.join(root, name)
 
+source_dir = 'C:\\Users\\User\\Portabolt\\Portabolt - Documents'
+destination_dir = 'C:\\Users\\User\\Documents\\Facturenfolder'
+
+for file in list_files(source_dir):
+    if file.endswith('.pdf'):
+        shutil.copy(file, destination_dir)

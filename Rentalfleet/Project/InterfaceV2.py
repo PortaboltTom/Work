2c926f2e60e8bf4396743d20234ee17b4402087c
PrintingTom <52293655+PrintingTom@users.noreply.github.com>
2023-01-03 19:53:09 +0100

    V2 begonnen
    V2 begonnen omdat deze een aantal aanpassingen heeft doorgevoerd, ben te moe om over de volgende stap na te denken, morgen weer een dag

--- Rentalfleet/Project/InterfaceV2.py
+++ Rentalfleet/Project/InterfaceV2.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timedelta
 from dataclasses import dataclass, field
-from typing import Type
+from typing import Type, List
 
 used_items = set()
 
@@ -7,6 +8,8 @@ used_items = set()
 class InverterRack:
     build_date: str
     rental_price: float = 0.0
+    start_date: datetime = None
+    end_date: datetime = None
 
     def __hash__(self):
         return hash(self.build_date)
@@ -15,7 +18,8 @@ class InverterRack:
 class BatteryRack:
     build_date: str
     rental_price: float = 0.0
-
+    start_date: datetime = None
+    end_date: datetime = None
 
     def __hash__(self):
         return hash(self.build_date)
@@ -24,6 +28,8 @@ class BatteryRack:
 class Trailer:
     build_date: str
     rental_price: float = 0.0
+    start_date: datetime = None
+    end_date: datetime = None
 
     def __hash__(self):
         return hash(self.build_date)
@@ -34,13 +40,15 @@ class Contract:
     removed_items: list = field(default_factory=list)
     total_cost: float = 0.0
     
-    def add_item(self, items: list):
+    def add_items(self, items: List[Type], start_date: datetime, end_date: datetime = None):
         added_items = []
         for item in items:
             if item in used_items:
                 print(f'{item.__class__.__name__} has already been used and will not be added to the contract')
             else:
                 used_items.add(item)
+                item.start_date = start_date
+                item.end_date = end_date
                 self.items.append(item)
                 self.total_cost += item.rental_price
                 added_items.append(item)
@@ -54,13 +62,21 @@ class Contract:
         self.total_cost -= item.rental_price
         self.removed_items.append(item)
         print(f'removing {item.__class__.__name__} from contract')
-        
-    def generate_invoice(self, period: int):
-        invoice_total = self.total_cost * period
-        for item in self.removed_items:
-            invoice_total += item.rental_price * period
-        print(f'Invoice total: {invoice_total:.2f}')
     
+    def generate_invoice(self, period: int):
+        invoice_total = 0.0
+        for item in self.items:
+            rental_period = period
+            if item.end_date:
+                rental_period = (item.end_date - item.start_date).days // 7
+            invoice_total += item.rental_price * rental_period
+        for item in self.removed_items:
+            rental_period = period
+            if item.end_date:
+                rental_period = (item.end_date - item.start_date).days // 7
+            invoice_total += item.rental_price * rental_period
+        print(f'Invoice total: {invoice_total:.2f}')
+        
     def print_items(self):
         for item in self.items:
             print(item.__class__.__name__)
@@ -70,32 +86,24 @@ battery_rack = BatteryRack(build_date='2022-01-02', rental_price=200.0)
 trailer = Trailer(build_date='2022-01-03', rental_price=300.0)
 
 contract = Contract()
-contract.add_item([inverter_rack, battery_rack, trailer, trailer])
-contract.remove_item(trailer)
-contract.print_items()
 
-'''
-contract.add_item(battery_rack)
-contract.add_item(trailer)
+# Add items to the contract with a start date and no end date
+start_date = datetime(2022, 1, 1)
+contract.add_items([inverter_rack, battery_rack, trailer], start_date=start_date)
 
-contract.generate_invoice(4)  # Output: Invoice total: 1200.00
+# Print the items in the contract
+contract.print_items()  # Output: InverterRack, BatteryRack, Trailer
 
-
-inverter_rack = InverterRack(build_date='2022-01-01', rental_price = 60)
-battery_rack = BatteryRack(build_date='2022-01-02', rental_price = 20)
-trailer = Trailer(build_date='2022-01-03', rental_price = 40)
-contract = Contract()
-
-
-contract.add_item(inverter_rack)
-contract.add_item(battery_rack)
-contract.add_item(trailer)
+# Remove an item from the contract
 contract.remove_item(inverter_rack)
 
-print(contract.items)  # Output: [InverterRack(build_date='2022-01-01'), BatteryRack(build_date='2022-01-02'), Trailer(build_date='2022-01-03')]
+# Generate an invoice for a 4 week period
+contract.generate_invoice(4)  # Output: Invoice total: 500.00
 
-try:
-    contract.add_item(inverter_rack)
-except ValueError as e:
-    print(e)  # Output: item has already been used
-'''
+# Add an item to the contract with a start date and end date
+start_date = datetime(2022, 1, 15)
+end_date = datetime(2022, 2, 5)
+contract.add_items([inverter_rack], start_date=start_date, end_date=end_date)
+
+# Generate an invoice for a 4 week period
+contract.generate_invoice(4)  # Output: Invoice total: 700.00
